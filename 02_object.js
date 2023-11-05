56a903ba37a42871d38c51b34978aea3d6f40dc4
nishant5harma <shiva5h4rm4@gmail.com>
2023-11-05 20:58:35 +0530

    second object lec added

---
+++ 02_object.js
@@ -0,0 +1,12 @@
+const users={}
+users.id=123,
+users.name='shiva'
+// console.log(users)
+const ru={
+    email:'shiva@apple.com',
+    fullname:{
+        username:'shiva sharma'
+    }
+}
+
+console.log(ru.fullname.username)
